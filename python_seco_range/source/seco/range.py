8dcd343319e058a2dc8205b050d355767cf1e666
Grier Johnson <grierj@gmail.com>
2011-11-22 11:21:11 -0800

    Improvements to the python library and cli * Allow input from stdin * Allow lists to be fed to expand() * Added collapse() convenience function * Removed accidental overrid of list() built-in in expand() * When a list is requested from expand(), return it as a sorted list

--- python_seco_range/source/seco/range.py
+++ python_seco_range/source/seco/range.py
@@ -1,6 +1,6 @@
 """
-  Library for querying the range webservice - http://github.com/ytoolshed/range
-  ebourget@linkedin.com
+Library for querying the range webservice - http://github.com/ytoolshed/range
+ebourget@linkedin.com
 """
 
 import urllib2
@@ -16,29 +16,36 @@ class Range(object):
     def __init__(self, host):
         self.host = host
 
-    def expand(self, expr, list=True):
-        if list:
-            url = 'http://%s/range/list?%s' % (self.host,
-                                               urllib2.quote(expr))
+    def expand(self, expr, ret_list=True):
+        if isinstance(expr, list):
+                expr = ','.join(expr)
+        if ret_list:
+            url = 'http://%s/range/list?%s' % (self.host, urllib2.quote(expr))
         else:
-            url = 'http://%s/range/expand?%s' % (self.host,
-                                                 urllib2.quote(expr))
-        req = urllib2.urlopen(url)
+            url = 'http://%s/range/expand?%s' % (self.host, urllib2.quote(expr))
+        req = None
+        try:
+            req = urllib2.urlopen(url)
+        except urllib2.URLError, e:
+            raise RangeException(e)
         code = req.getcode()
         if code != 200:
-            raise RangeException("Got %d response code from %s" %
-                                 (code, url))
+            raise RangeException("Got %d response code from %s" % (code, url))
         reqinfo = req.info()
         exception = reqinfo.getheader('RangeException')
         if exception:
             raise RangeException(exception)
-        if list:
+        if ret_list:
             expansion = []
             for line in req.readlines():
                 expansion.append(line.rstrip())
+            expansion.sort()
             return expansion
         else:
             return req.read()
+
+    def collapse(self, expr):
+        return self.expand(expr, ret_list=False)
 
 if __name__ == '__main__':
     try:
