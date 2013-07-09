3b6ef0f0118adbbf73417e3d65838092e571cf82
Evan Miller <eam@frap.net>
2013-07-08 18:00:42 -0700

    see what it installs

--- libcrange/source/testit.sh
+++ libcrange/source/testit.sh
@@ -9,6 +9,8 @@ $ENV{DESTDIR} = "$ENV{HOME}/prefix";
 $ENV{PATH} = "$ENV{DESTDIR}/usr/bin:$ENV{PATH}";
 $ENV{LD_LIBRARY_PATH} = "$ENV{DESTDIR}/usr/lib"; #FIXME should be lib64 for a 64bit build
 
+system "find $ENV{DESTDIR}
+
 # just md5sum outputs for now to make sure we're returning consistent data
 is( `crange -e foo100..10|md5sum`,
     "4364b988cf558c91d49786b83da444e4  -\n",
