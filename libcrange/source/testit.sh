ff51aa80f683fdf4c2141b3caf091fb0a61eebea
Evan Miller <eam@frap.net>
2013-07-08 17:51:55 -0700

    update tests

--- libcrange/source/testit.sh
+++ libcrange/source/testit.sh
@@ -10,14 +10,14 @@ $ENV{PATH} = "$ENV{DESTDIR}/usr/bin:$ENV{PATH}";
 $ENV{LD_LIBRARY_PATH} = "$ENV{DESTDIR}/usr/lib"; #FIXME should be lib64 for a 64bit build
 
 # just md5sum outputs for now to make sure we're returning consistent data
-is( `crange -e foo100..10|md5sum`, "4364b988cf558c91d49786b83da444e4  -\n", "foo100..10");
+is( `crange -e foo100..10|md5sum`,
+    "4364b988cf558c91d49786b83da444e4  -\n",
+    "foo100..10 # noconfig");
 
-# what a hack, need to be able to set perl paths properly
-# this is sooo ugly and temporary -- fix with a set var for inc path
 is(
    `crange  -c $ENV{DESTDIR}/etc/range.conf foo100..1 -e 2>&1 |md5sum`,
-   "6b26490a3dbd47f5fda3a7e6f21a8584  -\n",
-   "perl module loading w/o errors",
+   qq{foo100\nfoo101\n},
+   "foo100..1 # using range.conf",
   );
 
 is(
