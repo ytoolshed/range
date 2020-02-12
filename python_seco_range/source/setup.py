43eaae35dd9caec49e1a3f6e55ddbd8d10e272bd
btilton-gt <brian.tilton@groundtruth.com>
2020-02-12 14:34:52 -0800

    port python_seco_range to python3

--- python_seco_range/source/setup.py
+++ python_seco_range/source/setup.py
@@ -2,7 +2,6 @@
 
 from distutils.core import setup
 setup(name='seco/range',
-      version = '1.0',
+      version = '2.0',
       packages = [ 'seco', 'seco.range', 'seco.range.sync' ])
-      
-      
+
